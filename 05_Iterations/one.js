7783d7f16ed9314afdad27770b3cd0a32680936b
Rohit <rohitmandavkar3577@gmail.com>
2024-08-05 19:54:43 +0530

    Added Files One.js and Two.js in Iteration folder.

--- 05_Iterations/one.js
+++ 05_Iterations/one.js
@@ -31,10 +31,10 @@ let myArray = ["Flash", "Batman", "Superman"]
 
 for (let index = 1; index <= 20; index++) {
     if (index === 5) {
-        console.log(`Detected 5`);
+        // console.log(`Detected 5`);
         break;
     }
-    console.log(`Value of i is ${index}`);
+    // console.log(`Value of i is ${index}`);
 
         
 }
@@ -44,7 +44,7 @@ for (let index = 1; index <= 20; index++) {
         console.log(`Detected 5`);
         continue
     }
-    console.log(`Value of i is ${index}`);
+    // console.log(`Value of i is ${index}`);
 
         
 }
